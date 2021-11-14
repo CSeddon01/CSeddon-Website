45ed26e68583e331603140c877f99849a97cb2af
Christine Seddon <noddes@gmail.com>
2021-11-14 12:38:49 -0600

    Images

--- CSeddon-Website/script.js
+++ CSeddon-Website/script.js
@@ -1,4 +1,30 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
 
-document.querySelector("body").appendChild(h2);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
